f8f0cc78e900970bb16213a73843cd30785cc114
SoliTheUnholy <soheilzss82@gmail.com>
2025-12-16 03:01:33 +0330

    fuuuuck

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import ColorBends from "@/components/ColorBends";
+import Silk from "@/components/silk/silk";
 import React, { useRef } from "react";
 
 export default function Home() {
@@ -7,7 +7,13 @@ export default function Home() {
     return (
         <div className="relative w-full">
             <div ref={domRef} className="fixed h-screen w-full">
-            
+                <Silk
+                    speed={5}
+                    scale={2}
+                    color="#104e64"
+                    noiseIntensity={1.5}
+                    rotation={0}
+                />
             </div>
             <div className="absolute flex h-svh w-full items-center justify-center">
                 <span className="text-6xl font-bold">بزودی...</span>
