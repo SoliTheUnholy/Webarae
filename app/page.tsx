a91e854dae204bfd9817683cdcd1f60632135434
SoliTheUnholy <soheilzss82@gmail.com>
2025-12-16 02:50:19 +0330

    swap 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { GridScan } from "@/components/GridScan";
+import ColorBends from "@/components/ColorBends";
 import React, { useRef } from "react";
 
 export default function Home() {
@@ -7,7 +7,16 @@ export default function Home() {
     return (
         <div className="relative w-full">
             <div ref={domRef} className="fixed h-screen w-full">
-                
+            <ColorBends
+                    speed={1}
+                    scale={0.7}
+                    frequency={1}
+                    warpStrength={1}
+                    mouseInfluence={1}
+                    parallax={0.5}
+                    noise={0.1}
+                    transparent
+                />
             </div>
             <div className="absolute flex h-svh w-full items-center justify-center">
                 <span className="text-6xl font-bold">بزودی...</span>
