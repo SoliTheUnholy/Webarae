2e82c615ce65e3f42f5a495ee2f92f687f5f9572
SoliTheUnholy <soheilzss82@gmail.com>
2025-12-17 00:35:05 +0330

    lint my ass

--- components/GradualBlur.tsx
+++ components/GradualBlur.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import React, { CSSProperties, useEffect, useRef, useState, useMemo, PropsWithChildren } from 'react';
 import * as math from 'mathjs';
 
